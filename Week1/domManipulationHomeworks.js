9b3c215b955dcdb24505f69c108b6da9090b599d
yourusername <kunay69@hotmail.it>
2021-12-07 14:09:21 +0100

    inizio homeworks

---
+++ Week1/domManipulationHomeworks.js
@@ -0,0 +1,11 @@
+/*EX1: Add an h1 tag with the name of your shop
+
+EX2: Add a TABLE with the top 5 products of your shop
+
+EX3: Insert for each product an image, a title, a description and a price
+
+EX4: Add links to existing Amazon products for each item in the table
+
+EX5: Add a footer with the address and the name of your shop
+
+EX6: Add a message board where the user can type a message in it (no real posting/saving features required!)*/
