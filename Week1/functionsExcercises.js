c6c8d100db065d6644892f9fe993a424ab5272f5
yourusername <kunay69@hotmail.it>
2021-12-08 07:03:49 +0100

    updated exercise 3

--- Week1/functionsExcercises.js
+++ Week1/functionsExcercises.js
@@ -105,6 +105,9 @@ console.log(whoIsBigger(4, 7))
 
 console.log('\n Exercise 3')
 
+const splitMe = str => str.split(' ')
+console.log(splitMe('i am being splitted by the function'))
+
 /* EXERCISE 4
     Write a function called deleteOne which receives a string and a boolean as parameters.
     If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
