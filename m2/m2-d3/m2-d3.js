04d540b1fccba5f34ed29013ed2afcddd98e2b31
Mirko Sedda <75335906+MirkoSedda@users.noreply.github.com>
2021-12-15 17:11:41 +0100

    5pm work in progress

--- m2/m2-d3/m2-d3.js
+++ m2/m2-d3/m2-d3.js
@@ -14,12 +14,12 @@ let listItems = document.getElementsByTagName('li')
 
 secondInputRef = document.getElementById('secondInput')
 
-var defaultColor = '#0000ff'
+let defaultColor = '#0000ff'
 
-window.addEventListener('load', startup, false)
+//window.addEventListener('load', startup, false)
 
 function updateFirst(event) {
-  var p = document.querySelector('p')
+  let p = document.querySelector('p')
 
   if (p) {
     p.style.color = event.target.value
@@ -38,10 +38,16 @@ function startup() {
   firstInputRef.addEventListener('input', updateFirst, false)
   firstInputRef.addEventListener('change', updateAll, false)
   firstInputRef.select()
+  
+
+  secondInputRef = document.getElementById('secondInputId')
+  secondInputRef.value = defaultColor
+  secondInputRef.addEventListener('input', updateFirst, false)
+  secondInputRef.addEventListener('change', updateAll, false)
+  secondInputRef.select()
 }
 
-firstInputRef.addEventListener('input', updateFirst, false)
-firstInputRef.addEventListener('change', watchColorPicker, false)
+
 
 function watchColorPicker(event) {
   document.querySelectorAll('p').forEach(function (p) {
